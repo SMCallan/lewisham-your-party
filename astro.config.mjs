34fc1f41b95b01c9cc84980f914fd54ce0ea36c9
CallanSM <126923185+SMCallan@users.noreply.github.com>
2026-03-14 11:26:15 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // ADD THIS LINE BELOW (This is the missing piece for SEO)
+  site: 'https://lewishamyourparty.org.uk',
+  
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
